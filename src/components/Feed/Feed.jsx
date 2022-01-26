ce60dc7e6d329697b51d0546b07ffb973abc8725
Saeed Khan <saeed@skdevelopment.org>
2022-01-26 09:48:28 -0700

    basic navbar added

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import avatar from '../../assets/icons/avatar.png';
+import video from '../../assets/dummyImages/Ep. 13_ Take Care of Your Body Habits To Win Here and Hereafter Dr. Tesneem Alkiek.mp4'
+import './styles.css';
+
+const Feed = () => {
+  return ( 
+  <div className="container">
+      <div className="wrapper">
+          <div className="post-user">
+              <div className="icon">
+                  <img className="avatar" src={avatar} alt="" />
+              </div>
+              <span><strong>User</strong></span>
+          </div>
+          <div className="post-vid">
+              <video  className="video"src={video} controls></video>
+          </div>
+          <div className="post-desc">
+              <div className="desc">
+                  <span><strong>User </strong></span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus maiores nisi, quas laudantium ea voluptatum dignissimos officiis cumque et eos incidunt eveniet commodi harum repellendus quod modi quasi vel quis?
+              </div>
+          </div>
+      </div>
+  </div>
+  );
+};
+
+export default Feed;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Signup from './components/Signup/Signup.jsx';
 import Login from './components/Login/Login.jsx';
+import Feed from './components/Feed/Feed.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Route exact path="/" element={<App />} />
       <Route exact path="/signup" element={<Signup />} />
       <Route exact path="/login" element={<Login />} />
+      <Route exact path="/feed" element={<Feed />} />
     </Routes>
   </Router>,
   document.getElementById('root')
